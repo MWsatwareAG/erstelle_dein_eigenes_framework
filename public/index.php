9c5717bed0af98c2e276e2280b75690b8d16889b
Michael Wegener <wegener@satware.com>
2015-11-30 11:54:34 +0100

    Prepare for Symfony Routing component

--- public/index.php
+++ public/index.php
@@ -3,24 +3,26 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing;
 
 $request = Request::createFromGlobals();
-$response = new Response();
+$routes = include __DIR__ . '/../src/app.php';
 
-$map = array(
-    '/hello' => 'hello',
-    '/bye' => 'bye'
-);
+$context = new Routing\RequestContext();
+$context->fromRequest($request);
+$matcher = new Routing\Matcher\UrlMatcher($routes, $context);
 
-$path = $request->getPathInfo();
-
-if (isset($map[$path])) {
+try {
+    extract($matcher->match($request->getPathInfo()), EXTR_SKIP);
     ob_start();
-    extract($request->query->all(), EXTR_SKIP);
-    include sprintf(__DIR__ . '/../src/pages/%s.php', $map[$path]);
+    include sprintf(__DIR__ . '/../src/pages/%s.php', $_route);
+
     $response = new Response(ob_get_clean());
-} else {
-    $response = new Response(sprintf('%s nicht gefunden', $path), 404);
+} catch (Routing\Exception\ResourceNotFoundException $e) {
+    $response = new Response('Nicht gefunden', 404);
+} catch (Exception $e) {
+    $response = new Response('Ein Fehler ist aufgetreten');
 }
 
 $response->send();
+
