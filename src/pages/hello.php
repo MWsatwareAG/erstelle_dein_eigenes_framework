586ed997aa09e776b561eebc13bacfe7d0e0e6f0
Michael Wegener <wegener@satware.com>
2015-11-30 10:43:19 +0100

    Reformat code

--- src/pages/hello.php
+++ src/pages/hello.php
@@ -1,3 +1,2 @@
 <?php $input = $request->get('name', 'World') ?>
-
-    Hallo <?php echo  htmlspecialchars($input, ENT_QUOTES, 'UTF-8') ?>
+Hallo <?php echo  htmlspecialchars($input, ENT_QUOTES, 'UTF-8') ?>
